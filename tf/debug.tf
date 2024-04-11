3e05fd505798135d7e96a89da662aa6308b7eb11
Jonathan Cottrill <github@jonathancottrill.net>
2024-04-11 08:05:40 -0500

    Add debug output for table Hive columns/parition keys

--- tf/debug.tf
+++ tf/debug.tf
@@ -14,10 +14,36 @@
 # limitations under the License.
 ####
 
+resource "local_file" "debug-table-hive-json" {
+  for_each = var.debug-dir == "" ? {} : {
+    for name, resource in aws_glue_catalog_table.table : name => {
+      filename = "${var.debug-dir}/${resource.name}.hive.json"
+      json = jsonencode({
+        columns = [
+          for column in resource.storage_descriptor[0].columns: {
+            name = column.name
+            type = column.type
+          }
+        ]
+        partition_keys = [
+          for column in resource.partition_keys: {
+            name = column.name
+            type = column.type
+          }
+        ]
+      })
+    }
+  }
+
+  filename        = each.value.filename
+  content         = "${each.value.json}\n"
+  file_permission = "0664"
+}
+
 resource "local_file" "debug-view-trino-sql" {
   for_each = var.debug-dir == "" ? {} : {
     for name, resource in aws_glue_catalog_table.view : name => {
-      filename = "${var.debug-dir}/${name}.trino.sql"
+      filename = "${var.debug-dir}/${resource.name}.trino.sql"
       sql = jsondecode(
         base64decode(
           replace(
@@ -38,7 +64,7 @@ resource "local_file" "debug-view-trino-sql" {
 resource "local_file" "debug-view-trino-json" {
   for_each = var.debug-dir == "" ? {} : {
     for name, resource in aws_glue_catalog_table.view : name => {
-      filename = "${var.debug-dir}/${name}.trino.json"
+      filename = "${var.debug-dir}/${resource.name}.trino.json"
       json = base64decode(
         replace(
           resource.view_original_text,
@@ -57,7 +83,7 @@ resource "local_file" "debug-view-trino-json" {
 resource "local_file" "debug-view-hive-json" {
   for_each = var.debug-dir == "" ? {} : {
     for name, resource in aws_glue_catalog_table.view : name => {
-      filename = "${var.debug-dir}/${name}.hive.json"
+      filename = "${var.debug-dir}/${resource.name}.hive.json"
       json = jsonencode({
         columns = [
           for column in resource.storage_descriptor[0].columns: {
